e665df8c67d9624f769557ed9399f42c9d9bae16
priest-2105 <fawzybailey782@gmail.com>
2024-07-29 00:53:47 +0100

    update ✨✨

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: $process.env.ORIGIN,
     credentials: true,
   },
 });
