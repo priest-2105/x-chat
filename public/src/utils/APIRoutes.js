5375b203a61c11058abfc620a2c09ddd9c48223d
priest-2105 <fawzybailey782@gmail.com>
2024-07-29 02:49:15 +0100

    update ✨✨

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,5 @@
-export const host = "http://localhost:5000";
+// export const host = "http://localhost:5000";
+export const host = "https://x-chat-server.onrender.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
